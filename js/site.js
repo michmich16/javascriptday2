b611bb4f0918f064abc9db1d687a1f068b15bbf8
Michael P <michpouls@gmail.com>
2023-11-14 12:18:08 +0100

    js

---
+++ js/site.js
@@ -0,0 +1,23 @@
+// write cool JS here!!
+const myInfo = 'en variabel';
+const myBooks = ["The Catcher in the Rye",
+    "To Kill a Mockingbird",
+    "1984",
+    "Pride and Prejudice",
+    "The Great Gatsby",
+    "The Hobbit",
+    "Harry Potter and the Sorcerer's Stone",
+    "The Lord of the Rings",
+    "The Da Vinci Code",
+    "The Hunger Games"]
+
+    console.clear();
+    console.group("konsol opgave");
+    console.log("Jeg er en almindelig konsol log");
+    console.info("Jeg er en konsol info:",myInfo);
+    console.warn("jeg er en advarsel!");
+    console.error("Jeg er en fejl!!!");
+    console.table(myBooks);
+    console.time();
+    console.timeLog();
+    console.groupEnd();
